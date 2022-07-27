6625eafb154ab92c4df71640cbe3c3efb48d4b19
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-07-27 17:26:43 -0500

    added spring initializr project

---
+++ the-latest-java-api/src/main/java/com/thelatest/TheLatestJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.thelatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLatestJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLatestJavaApiApplication.class, args);
+	}
+
+}
---
+++ the-latest-java-api/src/test/java/com/thelatest/TheLatestJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thelatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheLatestJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
