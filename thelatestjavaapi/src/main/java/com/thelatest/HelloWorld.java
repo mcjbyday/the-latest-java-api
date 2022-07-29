ae0c190b8450c25a54e846c240d7f8de33222367
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-07-29 15:34:40 -0500

    first hello world class

---
+++ thelatestjavaapi/src/main/java/com/thelatest/HelloWorld.java
@@ -0,0 +1,13 @@
+package thelatestjavaapi.src.main.java.com.thelatest;
+
+public class HelloWorld {
+    //     class is blueprint that can be used to create new objects
+    // will often use class for static properties and methods without creating new objects
+
+//    main() method resembles process.argv in that it allows us to pass in arguments to the program.
+    public static void main(String[] args) {
+        System.out.println("hello fruity face heshe gingerbread");
+    }
+
+
+}
