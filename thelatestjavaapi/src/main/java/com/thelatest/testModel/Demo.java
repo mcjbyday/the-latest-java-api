5281e15fad77c5bf62b23fd0d9b74ebe12715073
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-08-01 11:47:55 -0500

    added getters and setters

--- thelatestjavaapi/src/main/java/com/thelatest/testModel/Demo.java
+++ thelatestjavaapi/src/main/java/com/thelatest/testModel/Demo.java
@@ -4,8 +4,26 @@ public class Demo {
     private String name;
     private int age;
 
+    // returns a string
+    public String getName() {
+        return name;
+    }
+    // doesn't return a value, uses camelCase for custom name, and custom arguments of specified type
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public Demo(String name, int age) {
         this.name = name;
         this.age = age;
+
     }
 }
