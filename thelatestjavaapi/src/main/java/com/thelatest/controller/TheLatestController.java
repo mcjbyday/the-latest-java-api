6ec842704ae857528597ea8a3350db20768ef6f7
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-08-03 12:23:37 -0500

    added edit post template

---
+++ thelatestjavaapi/src/main/java/com/thelatest/controller/TheLatestController.java
@@ -0,0 +1,162 @@
+package com.thelatest.controller;
+
+import com.thelatest.model.Comment;
+import com.thelatest.model.Post;
+import com.thelatest.model.User;
+import com.thelatest.repository.CommentRepository;
+import com.thelatest.repository.PostRepository;
+import com.thelatest.repository.UserRepository;
+import com.thelatest.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class TheLatestController {
+    @Autowired
+
+    PostRepository postRepository;
+
+    @Autowired
+
+    VoteRepository voteRepository;
+
+    @Autowired
+
+    UserRepository userRepository;
+
+    @Autowired
+
+    CommentRepository commentRepository;
+
+    @PostMapping("/users/login")
+    public String login(@ModelAttribute User user, Model model, HttpServletRequest request) throws Exception {
+
+        if ((user.getPassword().equals(null) || user.getPassword().isEmpty()) || (user.getEmail().equals(null) || user.getPassword().isEmpty())) {
+            model.addAttribute("notice", "Email address and password must be populated in order to login!");
+            return "login";
+        }
+
+        User sessionUser = userRepository.findUserByEmail(user.getEmail());
+
+        try {
+            // If sessionUser is invalid, running .equals() will throw an error
+            if (sessionUser.equals(null)) {
+
+            }
+            // We will catch an error and notify client that email address is not recognized
+        } catch (NullPointerException e) {
+            model.addAttribute("notice", "Email and password combination not recognized.");
+            return "login";
+        }
+
+        // Validate Password
+        String sessionUserPassword = sessionUser.getPassword();
+        boolean isPasswordValid = BCrypt.checkpw(user.getPassword(), sessionUserPassword);
+        if(isPasswordValid == false) {
+            model.addAttribute("notice", "Email and password combination not recognized.");
+            return "login";
+        }
+
+        sessionUser.setLoggedIn(true);
+        request.getSession().setAttribute("SESSION_USER", sessionUser);
+
+        return "redirect:/dashboard";
+    }
+
+    @PostMapping("/users")
+    public String signup(@ModelAttribute User user, Model model, HttpServletRequest request) throws Exception {
+
+        // if signup is successful, the API will redirect users to the login.html page to enter their new credentials
+        // if save fails, we'll throw an exception and redirect them to the login page to try again
+
+        if ((user.getUsername().equals(null) || user.getUsername().isEmpty()) || (user.getPassword().equals(null) || user.getPassword().isEmpty()) || (user.getEmail().equals(null) || user.getPassword().isEmpty())) {
+            model.addAttribute("notice", "In order to signup username, email address and password must be populated!");
+            return "login";
+        }
+
+        try {
+            // Encrypt password
+            user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
+            userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            model.addAttribute("notice", "Email address is not available! Please choose a different unique email address.");
+            return "login";
+        }
+
+        User sessionUser = userRepository.findUserByEmail(user.getEmail());
+
+        try {
+            if (sessionUser.equals(null)) {
+
+            }
+        } catch (NullPointerException e) {
+            model.addAttribute("notice", "User is not recognized!");
+            return "login";
+        }
+
+        sessionUser.setLoggedIn(true);
+        request.getSession().setAttribute("SESSION_USER", sessionUser);
+
+        return "redirect:/dashboard";
+    }
+
+    @PostMapping("/posts")
+    public String addPostDashboardPage(@ModelAttribute Post post, Model model, HttpServletRequest request) {
+
+        if ((post.getTitle().equals(null) || post.getTitle().isEmpty()) || (post.getPostUrl().equals(null) || post.getPostUrl().isEmpty())) {
+            return "redirect:/dashboardEmptyTitleAndLink";
+        }
+
+        if (request.getSession(false) == null) {
+            return "redirect:/login";
+        } else {
+            User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+            post.setUserId(sessionUser.getId());
+            postRepository.save(post);
+
+            return "redirect:/dashboard";
+        }
+
+    }
+
+    @PostMapping("/posts/{id}")
+    public String updatePostDashboardPage(@PathVariable int id, @ModelAttribute Post post, Model model, HttpServletRequest request) {
+
+        if (request.getSession(false) == null) {
+            model.addAttribute("user", new User());
+            return "redirect/dashboard";
+        } else {
+            Post tempPost = postRepository.getById(id);
+            tempPost.setTitle(post.getTitle());
+            postRepository.save(tempPost);
+
+            return "redirect:/dashboard";
+        }
+    }
+
+    @PostMapping("/comments")
+    public String createCommentCommentsPage(@ModelAttribute Comment comment, Model model, HttpServletRequest request) {
+
+        if (comment.getCommentText().isEmpty() || comment.getCommentText().equals(null)) {
+            return "redirect:/singlePostEmptyComment/" + comment.getPostId();
+        } else {
+            if (request.getSession(false) != null) {
+                User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+                comment.setUserId(sessionUser.getId());
+                commentRepository.save(comment);
+                return "redirect:/post/" + comment.getPostId();
+            } else {
+                return "login";
+            }
+        }
+    }
+}
+
